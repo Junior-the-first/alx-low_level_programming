0e8b52b8ab27a137d20e4b550b596086aadf828a
Junior-the-first <luthando@gmail.com>
2023-03-20 16:44:11 -0700

    Commit Update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ int main(void)
 	printf("Size of a long int. %d byte(S)\n", sizeof(long int));
 	printf("Size of a long long int. %d byte(S)\n", sizeof(long long int));
 	printf("Size of a float. %d byte(S)\n", sizeof(float));
-	return(0);
+	return (0);
 }
