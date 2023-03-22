19ddaa05b6989eea1058246e7427d613443dbaa4
Junior-the-first <luthando@gmail.com>
2023-03-21 19:27:49 -0700

    Commit Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
-#include <main.h>
-#include <unistd.h>
+#include "main.h"
 /**
- * Program that prints putchar
- * printing: c
- * Return 0
+ * main -Entry point
+ * Return: Always 0
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Add to integer values
+ * @n1: First number computed
+ * @n2: Second number computed
+ * Return: Integer value
+ */
+int add(int n1, int n2)
+{
+                return (n1 + n2);
+}
